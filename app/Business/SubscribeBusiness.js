d78511a32b90cf7d8b647a375fbfa737235e5bfa
RickMartian <rikmarciano@outlook.com.br>
2020-07-01 03:13:18 -0300

    #33 add id and address to getByCpfUser

--- app/Business/SubscribeBusiness.js
+++ app/Business/SubscribeBusiness.js
@@ -18,10 +18,10 @@ class SubscribeBusiness {
   async getByPk(data) {
     try {
       const subscribe = await Subscribe
-                          .query()
-                          .where('event_id', data.event_id)
-                          .where('cpf_user', data.cpf_user)
-                          .first()
+        .query()
+        .where('event_id', data.event_id)
+        .where('cpf_user', data.cpf_user)
+        .first()
 
       return subscribe
     } catch (error) {
@@ -32,7 +32,7 @@ class SubscribeBusiness {
   async delete(subscribe) {
     try {
       await Database
-          .raw('delete from subscribes where event_id = ? and cpf_user = ?',
+        .raw('delete from subscribes where event_id = ? and cpf_user = ?',
           [subscribe.event_id, subscribe.cpf_user])
       return
     } catch (error) {
@@ -43,9 +43,9 @@ class SubscribeBusiness {
   async getByCpfUser(cpf_user) {
     try {
       const events = await Database
-      .raw('select u.cpf cpf_user, e.name, e.start_date, e.end_date, e.description ' +
-      'from events e, subscribes s, users u where e.id = s.event_id and u.cpf = s.cpf_user and s.cpf_user = ?',
-      [cpf_user])
+        .raw('select u.cpf cpf_user, e.name, e.start_date, e.end_date, e.description, e.id, e.address ' +
+          'from events e, subscribes s, users u where e.id = s.event_id and u.cpf = s.cpf_user and s.cpf_user = ?',
+          [cpf_user])
       return events[0]
     } catch (error) {
       throw new Error(error)
